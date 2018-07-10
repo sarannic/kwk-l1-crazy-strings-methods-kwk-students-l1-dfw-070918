0726fdaa9087ca600364731015731b30988c3545
sarannic <snickershelton11@gmail.com>
2018-07-10 18:14:28 +0000

    automatically backed up by learn

--- crazy_string.rb
+++ crazy_string.rb
@@ -1,5 +1,9 @@
 # write your method here
 
-def crazy_strings()
-
+def crazy_strings(food, drink)
+  food = food.reverse.upcase
+  drink = drink.gsub("s", "z")
+  return "#{food} #{drink}"
 end
+
+crazy_strings("phone", "tea")
