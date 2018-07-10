2f4ddf34a407af451769fddd581d308129476904
sarannic <snickershelton11@gmail.com>
2018-07-10 18:16:45 +0000

    Done.

--- crazy_string.rb
+++ crazy_string.rb
@@ -1,5 +1,9 @@
 # write your method here
 
-def crazy_strings(a,b)
-
+def crazy_strings(food, drink)
+  food = food.upcase.reverse
+  drink = drink.swapcase.gsub("s", "z")
+  return "#{food} #{drink}"
 end
+
+crazy_strings("phone", "tea")
